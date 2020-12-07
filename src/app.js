66cfaadb737b1af6368e1d5eef3d4864841df101
akshaypandey44 <kumarakshaypandey@gmail.com>
2020-12-07 22:25:22 +0530

    final

--- src/app.js
+++ src/app.js
@@ -1,12 +1,37 @@
 
 const express = require('express');
 const app = express()
-
+const SubscribeModel = require('./models/subscribers');
 
 // Your code goes here
 
+app.get('/subscribers', async(req,res) => {
+    res.send(await SubscribeModel.find());
+});
 
-
+app.get('/subscribers/names', async(req,res) => {
+    const fullResults = await SubscribeModel.find();
+    const mappedResults = fullResults.map(doc => {
+        return {
+            name: doc.name,
+            SubscribeModel: doc.SubscribeModel
+        }
+    });
+    res.send(mappedResults);
+});
+app.get('/subscribers/:id',(req,res)=>{
+    const idToSearch = req.params.id;
+    try{
+        const doc = await.SubscribeModel.findOne({_id:idToSearch});
+        if(doc == null){
+            res.status(400).send({message: "Id not found"});
+        }else{
+            res.send(doc);
+        }
+    }catch(err){
+        res.status(400),send({message: "Incorrect id format"});
+    }
+})
 
 
 
